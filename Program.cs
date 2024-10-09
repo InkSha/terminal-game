e1aeb4b40c3a9e38e9a2106709f8e9b98eea4648
InkSha <x1a010@163.com>
2024-10-10 00:22:13 +0800

    simple demo

--- Program.cs
+++ Program.cs
@@ -1,8 +1,89 @@
-﻿using map;
+﻿using entity;
+using map;
 
 public class Program
 {
   public static void Main()
+  {
+    CreateWorld();
+    BaseEntity elf = new("elf");
+    Random random = new();
+
+    bool loop = true;
+
+    while (loop)
+    {
+      Console.WriteLine("你行走在大地上");
+      Console.WriteLine("怎么做?");
+      Console.WriteLine("[walk] 接着走\n[find] 搜寻周围\n[end] 结束这一切");
+      Console.WriteLine(new string('-', 30));
+
+      string? input = Console.ReadLine();
+
+      switch (input)
+      {
+        case "find":
+          if (random.Next(0, 100) > 50)
+          {
+            BaseEntity monster = new("monster");
+            bool war = true;
+            Console.WriteLine("你遭遇了怪物!");
+
+            while (war)
+            {
+              Console.WriteLine("怎么做?");
+              Console.WriteLine("[attack] 攻击\n[defense] 防御\n[run] 逃跑");
+              Console.WriteLine(new string('-', 30));
+              string? select = Console.ReadLine();
+
+              int damage;
+
+              switch (select)
+              {
+                case "attack":
+                  Console.WriteLine("攻击!");
+
+                  damage = elf.Damage(monster);
+
+                  Console.WriteLine($"你对 {monster.Name} 造成了 {damage} 伤害!");
+
+                  if (monster.IsDeath())
+                  {
+                    Console.WriteLine($"你击败了 {monster.Name} !");
+                    war = false;
+                    loop = false;
+                  }
+
+                  break;
+                case "defense":
+                  Console.WriteLine("防御!");
+
+                  damage = monster.Damage(elf);
+
+                  Console.WriteLine($"{monster.Name} 对你造成了 {damage} 伤害!");
+                  break;
+                case "run":
+                  Console.WriteLine("逃跑了!");
+                  war = false;
+                  loop = false;
+                  break;
+              }
+            }
+          }
+          else
+          {
+            Console.WriteLine("一无所获");
+          }
+          break;
+        case "end":
+          loop = false;
+          break;
+      }
+    }
+
+  }
+
+  public static void CreateWorld()
   {
     Area main = new("main world");
     Area land = new("land");
--- entity/Base.cs
+++ entity/Base.cs
@@ -4,13 +4,14 @@ namespace entity;
 
 public class BaseEntity(string name)
 {
-  protected string Name { get; set; } = name;
+  public string Name { get; set; } = name;
   protected int Level { get; set; } = 1;
   protected int Health { get; set; } = 100;
-  protected int Attack { get; set; } = 5;
-  protected int Defense { get; set; } = 20;
-  protected int MissRate { get; set; } = 5;
+  protected int Attack { get; set; } = 10;
+  protected int Defense { get; set; } = 5;
+  protected int MissRate { get; set; } = 10;
   protected int HitRate { get; set; } = 0;
+  protected bool Life { get; set; } = true;
 
   public string ToJson()
   {
@@ -41,5 +42,24 @@ public class BaseEntity(string name)
       filename = path;
     }
     File.WriteAllText(filename, content);
+  }
+
+  public bool IsDeath()
+  {
+    return !Life;
+  }
+
+  public int Damage(BaseEntity entity)
+  {
+    int damage = Attack - (entity.Defense / 2);
+
+    entity.Health -= damage;
+
+    if (entity.Health <= 0)
+    {
+      entity.Life = false;
+    }
+
+    return damage;
   }
 }
